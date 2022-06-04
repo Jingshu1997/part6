4f8b42c70296d546f550d7a2627a508a72d967e6
ZHANG_Jingshu <zhangjingshu1010@gmail.com>
2022-06-04 23:48:38 +0200

    changes to bad/ok/zero

--- unicafe-redux/src/index.js
+++ unicafe-redux/src/index.js
@@ -6,21 +6,45 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
+
+  console.log(store.getState())
+
   const good = () => {
     store.dispatch({
       type: 'GOOD'
     })
   }
 
+  console.log(store.getState())
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+  console.log(store.getState())
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+  console.log(store.getState())
+  const zero = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+  console.log(store.getState())
+
+
   return (
     <div>
       <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={ok}>ok</button> 
+      <button onClick={bad}>bad</button>
+      <button onClick={zero}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
