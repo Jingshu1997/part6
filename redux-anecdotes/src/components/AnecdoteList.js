dc499ab0ae07a356f88263cd71a890676b988ac5
ZHANG_Jingshu <zhangjingshu1010@gmail.com>
2022-06-05 13:43:02 +0200

    last

---
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
+import { addVote } from '../reducers/anecdoteReducer'
+import {  setNotification  } from '../reducers/notificationReducer'
+import '../index.css'
+const AnecdoteList = (props) => {
+  const vote = (anecdote) => {
+    const anecdotevalue= props.anecdotes.find(n => n.id === anecdote)
+   
+    props.addVote(anecdote)
+    props.setNotification (`you voted '${anecdotevalue.content}'`, 5)
+  }
+
+  return (
+    <div id='list'>
+      {props.anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+
+      )}
+    </div>
+  )
+}
+const anecdotesToShow = ({anecdotes, filter}) => {
+
+  return [...anecdotes]
+    .sort((anecdote1, anecdote2) => anecdote2.votes - anecdote1.votes)
+    .filter(anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase()))
+}
+
+const mapStateToProps = (state) => {
+  return {
+    anecdotes: anecdotesToShow(state),
+    filter: state.filter
+  }
+}
+
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addVote: value=> dispatch(addVote(value)),
+    setNotification: (content, timeout) => dispatch(setNotification(content, timeout))
+   }
+}
+
+export default connect(
+  mapStateToProps, mapDispatchToProps
+  )(AnecdoteList)
---
+++ redux-anecdotes/src/components/AnecdoterForm.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+import { setNotification} from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
+const AnecdoteForm = (props) => {
+  const dispatch = useDispatch()
+
+  const addAnecdote = async (event) => {
+    event.preventDefault()
+    const  content  = event.target.anecdote.value
+    event.target.anecdote.value = ''
+   props.createAnecdote(content)
+    console.log(content)
+   props.setNotification(`new anecdote '${content}'`, 5)
+   
+  }
+
+  return (
+    <>
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div><input name='anecdote' /></div>
+        <button>create</button>
+   
+      </form>
+    </>
+  )
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    createAnecdote: value => dispatch(createAnecdote(value)),
+    setNotification: (content, timeout) => dispatch(setNotification(content, timeout))
+   }
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(AnecdoteForm)
+
+/* const mapDispatchToProps = {
+  filterChange
+}
+
+
+export default connect(
+  null, 
+  mapDispatchToProps
+  )(Filter)
+   */
