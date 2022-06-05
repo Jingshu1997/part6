059ff3a6f7fb0647b358a1c0fba0f36fb804ca0d
ZHANG_Jingshu <zhangjingshu1010@gmail.com>
2022-06-05 17:01:13 +0200

    addf

--- unicafe-redux/src/index.js
+++ unicafe-redux/src/index.js
@@ -8,43 +8,41 @@ const store = createStore(reducer)
 const App = () => {
 
   console.log(store.getState())
-
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
-
-  console.log(store.getState())
-  const ok = () => {
-    store.dispatch({
-      type: 'OK'
-    })
-  }
-  console.log(store.getState())
-  const bad = () => {
-    store.dispatch({
-      type: 'BAD'
-    })
-  }
-  console.log(store.getState())
-  const zero = () => {
-    store.dispatch({
-      type: 'ZERO'
-    })
-  }
-  console.log(store.getState())
+  
+    const good = () => {
+      store.dispatch({
+        type: 'GOOD'
+      })
+    }
+    console.log(store.getState())
+    const ok = () => {
+      store.dispatch({
+        type: 'OK'
+      })
+    }
+    console.log(store.getState())
+    const bad = () => {
+      store.dispatch({
+        type: 'BAD'
+      })
+    } 
+    console.log(store.getState())
+    const reset = () => {
+      store.dispatch({
+        type: 'ZERO'
+      })
+    }
 
 
   return (
-    <div>
-      <button onClick={good}>good</button> 
-      <button onClick={ok}>ok</button> 
+   <div>
+      <button onClick={good}>good</button>
+      <button onClick={ok}>neutral</button>
       <button onClick={bad}>bad</button>
-      <button onClick={zero}>reset stats</button>
+      <button onClick={reset}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok {store.getState().ok}</div>
-      <div>bad {store.getState().good}</div>
+      <div>neutral {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
--- unicafe-redux/src/reducer.js
+++ unicafe-redux/src/reducer.js
@@ -8,16 +8,16 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
-    case 'ZERO':
-      return state
-    default: return state
-  }
-  
+        return {...state, good: state.good + 1}
+      case 'OK':
+        return {...state, ok: state.ok + 1}
+      case 'BAD':
+        return {...state, bad: state.bad + 1}
+      case 'ZERO':
+        return initialState
+      default:
+          return state
+    }
 }
 
 export default counterReducer
